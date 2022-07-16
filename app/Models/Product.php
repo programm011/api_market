1f1eb63b6b3db01d779a382e893e822592f72c10
Nizomiddin Zaripov <38242828+programm011@users.noreply.github.com>
2022-07-16 19:16:00 +0500

    added morhp table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public const PRODUCT_MAIN_IMAGE_RESOURCES = 'PRODUCT_MAIN_IMAGE_RESOURCES';
+    public const PRODUCT_VIDEO_RESOURCES = 'PRODUCT_VIDEO_RESOURCES';
+    public const PRODUCT_IMAGES_RESOURCES = 'PRODUCT_IMAGES_RESOURCES';
 
     public function category()
     {
@@ -17,5 +23,20 @@ class Product extends Model
     public function scopeActive($query)
     {
         return $query->where('is_active', true);
+    }
+
+    public function mainImage(): MorphOne
+    {
+        return $this->morphOne(Resource::class, 'resource')->where('additional_identifier', self::PRODUCT_MAIN_IMAGE_RESOURCES);
+    }
+
+    public function video(): MorphOne
+    {
+        return $this->morphOne(Resource::class, 'resource')->where('additional_identifier', self::PRODUCT_VIDEO_RESOURCES);
+    }
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Resource::class, 'resource')->where('additional_identifier', self::PRODUCT_IMAGES_RESOURCES);
     }
 }
