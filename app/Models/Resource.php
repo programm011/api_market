91dbc5c8a97339ebb7587e6738b76be060fa8452
Nizomiddin Zaripov <38242828+programm011@users.noreply.github.com>
2022-07-16 19:25:18 +0500

    resource refactor

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\URL;
 
 class Resource extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'additional_identifier',
+        'type',
+        'path',
+        'resource'
+    ];
+
+    protected $hidden = ['additional_identifier', 'resource_type', 'resource_id', 'created_at', 'updated_at'];
+
+
+    public function getPathAttribute(): ?string
+    {
+        return URL::to($this->path);
+    }
+
+    /**
+     * @return MorphTo
+     */
+    public function resource(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
